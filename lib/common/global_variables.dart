2ca866667c73b68de99e73617e5a6c3526e45bee
Muhammad rico <ricoexse@gmail.com>
2023-12-27 04:23:07 +0700

    menambahkan asset dan configurasi pages

---
+++ lib/common/global_variables.dart
@@ -0,0 +1,3 @@
+class GlobalVariables {
+  static const baseUrl = 'https://9127-2404-8000-1028-10fb-b831-c98c-f209-e5ad.ngrok-free.app';
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_seller_app_fic7/main.dart';
+import 'package:flutter_seller_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
